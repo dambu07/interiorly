11e317e167b8277be6eec0a947a8b5ed20f69e5e
Julian S <200318@studierende.htl-donaustadt.at>
2024-03-02 22:56:38 +0100

    chore: global fix for db instances in dev
    fixed pooler connection timeout caused by instantiating a new drizzle client on every hot reload
    
    see: https://github.com/drizzle-team/drizzle-orm/issues/928#issuecomment-1739105895

--- lib/supabase/db.ts
+++ lib/supabase/db.ts
@@ -6,9 +6,26 @@ const { DATABASE_URL } = process.env;
 
 if (!DATABASE_URL) {
   console.log("🔴 No database URL");
+  process.exit(1);
 }
 
-const client = postgres(DATABASE_URL as string, { max: 3, prepare: false });
-const db = drizzle(client, { schema });
+declare module global {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
 
+let postgresSqlClient;
+
+if (process.env.NODE_ENV !== "production") {
+  if (!global.postgresSqlClient) {
+    global.postgresSqlClient = postgres(DATABASE_URL, {
+      max: 3,
+      prepare: false,
+    });
+  }
+  postgresSqlClient = global.postgresSqlClient;
+} else {
+  postgresSqlClient = postgres(DATABASE_URL, { max: 3, prepare: false });
+}
+
+const db = drizzle(postgresSqlClient, { schema });
 export default db;
